f816765b3a45198b14326fd6069098f60f3f4385
Lee SeungHun <q6801@naver.com>
2021-10-27 18:31:32 +0900

    feat: 기본 spring boot 설정

---
+++ src/main/java/com/example/mediacommunity/MediaCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediacommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaCommunityApplication.class, args);
+	}
+
+}
