37ed4885c3c0a19858ef19703947a8020ef56b7e
Lee SeungHun <q6801@naver.com>
2021-11-10 16:16:12 +0900

    feat: 로그인 여부에 따른 home view 변경

--- src/main/java/com/example/mediacommunity/controller/home/HomeController.java
+++ src/main/java/com/example/mediacommunity/controller/home/HomeController.java
@@ -1,12 +1,21 @@
 package com.example.mediacommunity.controller.home;
 
+import com.example.mediacommunity.constant.SessionConst;
+import com.example.mediacommunity.domain.member.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home() {
-        return "index";
+    public String home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)
+                               Member member, Model model) {
+        if (member == null) {
+            return "home/home";
+        }
+        model.addAttribute("nickname", member.getNickname());
+        return "home/loginHome";
     }
 }
