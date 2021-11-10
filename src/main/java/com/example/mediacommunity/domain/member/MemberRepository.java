de9fd6a6695bacdccd1b7611f8c3f6f94cbf22cf
Lee SeungHun <q6801@naver.com>
2021-11-10 16:09:35 +0900

    feat: findByLoginId 추가

--- src/main/java/com/example/mediacommunity/domain/member/MemberRepository.java
+++ src/main/java/com/example/mediacommunity/domain/member/MemberRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 public interface MemberRepository {
     Member save(Member member);
     Member findById(Long id);
+    Member findByLoginId(String loginId);
     Member findByNickName(String nickName);
     List<Member> findAll();
     void clear();
