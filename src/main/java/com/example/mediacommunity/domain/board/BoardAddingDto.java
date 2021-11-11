3160750b8ac95240143e575d41ddbca707d25733
Lee SeungHun <q6801@naver.com>
2021-11-11 13:26:22 +0900

    feat: BoardAddingDt에 bean validation 추가

--- src/main/java/com/example/mediacommunity/domain/board/BoardAddingDto.java
+++ src/main/java/com/example/mediacommunity/domain/board/BoardAddingDto.java
@@ -2,13 +2,19 @@ package com.example.mediacommunity.domain.board;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
 import java.sql.Timestamp;
 
 @Data
 public class BoardAddingDto {
+    @NotBlank
     private String content;
+
+    @Positive
     private Long writerId;
 
+    public BoardAddingDto() {}
     public BoardAddingDto(String content, Long writerId) {
         this.content = content;
         this.writerId = writerId;
