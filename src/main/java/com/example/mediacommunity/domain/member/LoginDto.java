33c6e9e5d812bf293b191f5f84fb2d451e55b33f
Lee SeungHun <q6801@naver.com>
2021-11-11 23:32:10 +0900

    feat: bean validation 추가

--- src/main/java/com/example/mediacommunity/domain/member/LoginDto.java
+++ src/main/java/com/example/mediacommunity/domain/member/LoginDto.java
@@ -3,10 +3,16 @@ package com.example.mediacommunity.domain.member;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 public class LoginDto {
+    @NotBlank
     private String loginId;
+
+    @NotBlank
     private String password;
-    private String nickname;
+
+    public LoginDto() {}
 }
