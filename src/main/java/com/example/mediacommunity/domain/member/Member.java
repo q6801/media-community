b25ba1427dbcb868cb34886ef6dc3d6b99ca0d39
Lee SeungHun <q6801@naver.com>
2021-11-14 21:59:05 +0900

    feat: member 생성자 추가

--- src/main/java/com/example/mediacommunity/domain/member/Member.java
+++ src/main/java/com/example/mediacommunity/domain/member/Member.java
@@ -2,15 +2,14 @@ package com.example.mediacommunity.domain.member;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-//@Getter
-//@Setter
+
 @AllArgsConstructor
 @Data
 public class Member {
     private String loginId;
     private String password;
     private String nickname;
+
+    public Member() {};
 }
