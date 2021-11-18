916b7be7ca6cc98e5eb3c81a6ceebbc16bf513eb
Lee SeungHun <q6801@naver.com>
2021-11-18 12:53:26 +0900

    feat: board의 title 추가

--- src/main/java/com/example/mediacommunity/domain/board/Board.java
+++ src/main/java/com/example/mediacommunity/domain/board/Board.java
@@ -12,12 +12,14 @@ public class Board {
     private Timestamp updatedAt;
     private String writerId;
     private int viewCnt;
+    private String title;
 
-    public Board(String content, Timestamp createdAt, Timestamp updatedAt, String writerId, int viewCnt) {
+    public Board(String content, Timestamp createdAt, Timestamp updatedAt, String writerId, int viewCnt, String title) {
         this.content = content;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
         this.writerId = writerId;
         this.viewCnt = viewCnt;
+        this.title = title;
     }
 }
