98728ff43d0293a73d16b7d1d9840a3127128b9c
Lee SeungHun <q6801@naver.com>
2021-11-18 17:14:40 +0900

    feat: 페이지 숫자를 유동적으로 변경되게 만듦
    #8 issue

--- src/main/java/com/example/mediacommunity/service/Pagination.java
+++ src/main/java/com/example/mediacommunity/service/Pagination.java
@@ -7,27 +7,29 @@ import org.springframework.stereotype.Component;
 @Component
 public class Pagination {
     private  int onePageBoardsNum = 10;
-    private int rangeSize = 10;
+    private int rangeSize = 7;
 
     private int page;
-    private int startingRangeNum;
     private int totalBoardsNum;
     private int totalPagesNum;
     private int startingBoardNumInPage;
+    private int startPage;
     private int endPage;
     private boolean prev;
     private boolean next;
 
-    public void pageInfo(int page, int startingRangeNum, int totalBoardsNum) {
+    public void pageInfo(int page, int totalBoardsNum) {
         this.page = page;
-        this.startingRangeNum = startingRangeNum;
         this.totalBoardsNum = totalBoardsNum;
 
+        int left = page - (int)Math.ceil((double)rangeSize / 2) + 1;
+        this.startPage = (left > 0) ? left: 1;
+
         this.totalPagesNum = (int) Math.ceil((double)totalBoardsNum / onePageBoardsNum);
-        this.endPage = startingRangeNum * rangeSize;
+        this.endPage = this.startPage + rangeSize - 1;
         this.startingBoardNumInPage = (page - 1) * onePageBoardsNum;
-        this.prev = (startingRangeNum == 1) ? false : true;
-        this.next = (endPage > totalPagesNum) ? false : true;
+        this.prev = (this.startPage == 1) ? false : true;
+        this.next = (endPage >= totalPagesNum) ? false : true;
 
         if (this.endPage > this.totalPagesNum) {
             this.endPage = this.totalPagesNum;
