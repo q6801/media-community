624da8fe56f96b533cddd2e25adfbe17e840e811
Lee SeungHun <q6801@naver.com>
2021-11-18 12:53:33 +0900

    feat: board의 title 추가

--- src/main/java/com/example/mediacommunity/domain/board/BoardAddingDto.java
+++ src/main/java/com/example/mediacommunity/domain/board/BoardAddingDto.java
@@ -9,10 +9,14 @@ import java.sql.Timestamp;
 @Data
 public class BoardAddingDto {
     @NotBlank
+    private String title;
+
+    @NotBlank
     private String content;
 
     public BoardAddingDto() {}
-    public BoardAddingDto(String content, String writerId) {
+    public BoardAddingDto(String title, String content, String writerId) {
+        this.title = title;
         this.content = content;
     }
 }
--- src/main/java/com/example/mediacommunity/domain/board/BoardEditingDto.java
+++ src/main/java/com/example/mediacommunity/domain/board/BoardEditingDto.java
@@ -7,14 +7,15 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class BoardEditingDto {
     @NotBlank
-    private String content;
+    private String title;
 
     @NotBlank
-    private String writerId;
+    private String content;
+
 
     public BoardEditingDto() {}
-    public BoardEditingDto(String content, String writerId) {
+    public BoardEditingDto(String title, String content, String writerId) {
+        this.title = title;
         this.content = content;
-        this.writerId = writerId;
     }
 }
