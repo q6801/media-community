9b4f639ee91b1627357d71c8e271cb7efe495713
Lee SeungHun <q6801@naver.com>
2021-11-18 10:40:10 +0900

    feat: pagination과 viewCnt, delete을 위한 기능 추가

--- src/main/java/com/example/mediacommunity/domain/board/BoardRepository.java
+++ src/main/java/com/example/mediacommunity/domain/board/BoardRepository.java
@@ -1,11 +1,17 @@
 package com.example.mediacommunity.domain.board;
 
+import com.example.mediacommunity.service.Pagination;
+
 import java.util.List;
 
 public interface BoardRepository {
     Board save(Board board);
     Board findById(Long id);
     List<Board> findByWriterId(String writerId);
+    List<Board> findBoards(Pagination pagination);
     List<Board> findAll();
     void update(Long BoardIdx, Board updateParam);
+    void increaseViewCnt(Long id, int viewCnt);
+    void delete(Long id);
+    int getTotalBoardsNum();
 }
