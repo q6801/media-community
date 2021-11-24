72ae730f0b473d9b6b023596196d84f8d323df7f
Lee SeungHun <q6801@naver.com>
2021-11-25 01:19:51 +0900

    feat: 로그인을 위한 서비스계층으로 spring security에 사용

---
+++ src/main/java/com/example/mediacommunity/service/member/UserService.java
@@ -0,0 +1,42 @@
+package com.example.mediacommunity.service.member;
+
+import com.example.mediacommunity.domain.member.Member;
+import com.example.mediacommunity.domain.member.MemberRepository;
+import com.example.mediacommunity.domain.member.SignUpDto;
+import com.example.mediacommunity.domain.member.UserInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        Member member = memberRepository.findByLoginId(loginId).orElseThrow();
+        UserInfo userInfo = new UserInfo(member);
+        return userInfo;
+    }
+
+    public void save(SignUpDto signUpDto, BindingResult bindingResult) {
+        Optional<Member> duplicatedId = memberRepository.findByLoginId(signUpDto.getLoginId());
+        Optional<Member> duplicatedName = memberRepository.findByNickName(signUpDto.getNickname());
+
+        if (bindingResult.hasErrors()) return;                              // blank가 있는 경우
+        if (duplicatedId != null && duplicatedName != null) {                 // id가 중복되지 않는 경우
+            memberRepository.save(new Member(signUpDto.getLoginId(),
+                    passwordEncoder.encode(signUpDto.getPassword()), signUpDto.getNickname()));
+        } else {
+            bindingResult.reject("signUpFail");
+        }
+    }
+}
