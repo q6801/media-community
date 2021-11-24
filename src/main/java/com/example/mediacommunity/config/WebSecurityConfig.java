c680c3ca06e1ffb5598d0818d7f8a0217009f6bd
Lee SeungHun <q6801@naver.com>
2021-11-25 01:20:36 +0900

    feat: spring security를 위한 설정

---
+++ src/main/java/com/example/mediacommunity/config/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.mediacommunity.config;
+
+import com.example.mediacommunity.service.member.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.formLogin().disable().logout().disable();
+        http.authorizeRequests()
+                .antMatchers("/login", "/signup", "/", "/boards", "/boards/{^[0-9]+$}").permitAll()
+                .antMatchers("/boards/**").authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .usernameParameter("loginId")
+                .and()
+            .logout()
+                .logoutUrl("/logout")
+                .invalidateHttpSession(true)
+                .logoutSuccessUrl("/");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+    }
+}
