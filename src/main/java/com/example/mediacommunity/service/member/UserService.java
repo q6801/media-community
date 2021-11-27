9486970dcc6b97d6070d9e9176b8a9b66b7067b6
Lee SeungHun <q6801@naver.com>
2021-11-28 00:53:06 +0900

    feat: throw exception을 변경

--- src/main/java/com/example/mediacommunity/service/member/UserService.java
+++ src/main/java/com/example/mediacommunity/service/member/UserService.java
@@ -5,6 +5,7 @@ import com.example.mediacommunity.domain.member.MemberRepository;
 import com.example.mediacommunity.domain.member.SignUpDto;
 import com.example.mediacommunity.domain.member.UserInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -22,7 +23,8 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
-        Member member = memberRepository.findByLoginId(loginId).orElseThrow();
+        Member member = memberRepository.findByLoginId(loginId).orElseThrow(() ->
+                new InternalAuthenticationServiceException("UserService return null member, which means id is wrong"));
         UserInfo userInfo = new UserInfo(member);
         return userInfo;
     }
@@ -32,7 +34,7 @@ public class UserService implements UserDetailsService {
         Optional<Member> duplicatedName = memberRepository.findByNickName(signUpDto.getNickname());
 
         if (bindingResult.hasErrors()) return;                              // blank가 있는 경우
-        if (duplicatedId != null && duplicatedName != null) {                 // id가 중복되지 않는 경우
+        if (duplicatedId.isEmpty() && duplicatedName.isEmpty()) {                 // id가 중복되지 않는 경우
             memberRepository.save(new Member(signUpDto.getLoginId(),
                     passwordEncoder.encode(signUpDto.getPassword()), signUpDto.getNickname()));
         } else {
