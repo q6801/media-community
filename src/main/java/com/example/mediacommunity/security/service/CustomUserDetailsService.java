b9906c0ac62cb0d2ab49b707eb4af5c8a438113c
Lee SeungHun <q6801@naver.com>
2021-12-04 23:25:16 +0900

    feat: member의 속성으로 provider 추가

--- src/main/java/com/example/mediacommunity/security/service/CustomUserDetailsService.java
+++ src/main/java/com/example/mediacommunity/security/service/CustomUserDetailsService.java
@@ -3,7 +3,8 @@ package com.example.mediacommunity.security.service;
 import com.example.mediacommunity.community.domain.member.Member;
 import com.example.mediacommunity.community.domain.member.MemberRepository;
 import com.example.mediacommunity.community.domain.member.SignUpDto;
-import com.example.mediacommunity.community.domain.member.UserInfo;
+import com.example.mediacommunity.security.userInfo.UserInfo;
+import com.example.mediacommunity.security.BadProviderException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,6 +26,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
         Member member = memberRepository.findByLoginId(loginId).orElseThrow(() ->
                 new InternalAuthenticationServiceException("UserService return null member, which means id is wrong"));
+        if(!member.getProvider().equals("local")) {
+            throw new BadProviderException("provider is not local");
+        }
         UserInfo userInfo = new UserInfo(member);
         return userInfo;
     }
@@ -36,7 +40,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         if (bindingResult.hasErrors()) return;                              // blank가 있는 경우
         if (duplicatedId.isEmpty() && duplicatedName.isEmpty()) {                 // id가 중복되지 않는 경우
             memberRepository.save(new Member(signUpDto.getLoginId(),
-                    passwordEncoder.encode(signUpDto.getPassword()), signUpDto.getNickname()));
+                    passwordEncoder.encode(signUpDto.getPassword()), signUpDto.getNickname(), "local"));
         } else {
             bindingResult.reject("signUpFail");
         }
