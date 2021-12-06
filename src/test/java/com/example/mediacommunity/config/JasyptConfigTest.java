8c2db5ac76d1be45176ff39922ff14de60be45f4
Lee SeungHun <q6801@naver.com>
2021-12-06 12:23:49 +0900

    feat: jasypt를 이용한 application.yml의 암호화 추가

---
+++ src/test/java/com/example/mediacommunity/config/JasyptConfigTest.java
@@ -0,0 +1,25 @@
+package com.example.mediacommunity.config;
+
+import org.assertj.core.api.Assertions;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JasyptConfigTest {
+    @Test
+    public void jasypt_encrypt_decrypt_test() {
+        String plainText = "hXjgvqFdfB6ELDfpKVhJKhb7FeSQBNdn";
+
+        StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
+        jasypt.setPassword("password");
+        jasypt.setAlgorithm("PBEWithMD5AndDES");
+
+        String encryptedText = jasypt.encrypt(plainText);
+        String decryptedText = jasypt.decrypt(encryptedText);
+
+        System.out.println("encryptedText = " + encryptedText);
+
+        Assertions.assertThat(plainText).isEqualTo(decryptedText);
+    }
+}
