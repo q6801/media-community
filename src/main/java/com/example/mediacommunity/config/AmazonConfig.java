35a7e7b2cedacb239543e4af660a89b462743fa6
Lee SeungHun <q6801@naver.com>
2021-12-09 15:58:00 +0900

    feat: amazon s3를 IAM 아이디, 비밀번호로 연동함

---
+++ src/main/java/com/example/mediacommunity/config/AmazonConfig.java
@@ -0,0 +1,34 @@
+package com.example.mediacommunity.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String AWS_ACCESS_KEY_ID;               // IAM id, key
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String AWS_SECRET_ACCESS_KEY;
+
+    @Bean
+    public AmazonS3 S3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                AWS_ACCESS_KEY_ID,
+                AWS_SECRET_ACCESS_KEY
+        );
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.AP_NORTHEAST_2)
+                .build();
+    }
+
+}
