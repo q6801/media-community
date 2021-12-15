b3af80c49871b45ef2971291904e59ee13457272
Lee SeungHun <q6801@naver.com>
2021-12-15 22:50:49 +0900

    feat: Member entity class에 jpa를 적용

--- src/main/java/com/example/mediacommunity/community/domain/member/Member.java
+++ src/main/java/com/example/mediacommunity/community/domain/member/Member.java
@@ -3,10 +3,16 @@ package com.example.mediacommunity.community.domain.member;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name="Members")
 public class Member {
+    @Id
     private String loginId;
     private String password;
     private String nickname;
