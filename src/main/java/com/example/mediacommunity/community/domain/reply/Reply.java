5b3c30ced62bf997921c32c5f621dd62874c7b3d
Lee SeungHun <q6801@naver.com>
2021-12-18 00:20:40 +0900

    feat: reply class에 jpa를 적용

--- src/main/java/com/example/mediacommunity/community/domain/reply/Reply.java
+++ src/main/java/com/example/mediacommunity/community/domain/reply/Reply.java
@@ -1,21 +1,69 @@
 package com.example.mediacommunity.community.domain.reply;
 
+import com.example.mediacommunity.community.domain.board.Board;
+import com.example.mediacommunity.community.domain.member.Member;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
-@Data
+@Getter
+@Entity
+@NoArgsConstructor
 public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long boardId;
+//    private Long boardId;
     private String content;
-    private String replyer;
+//    private String replyer;
+    @UpdateTimestamp
     private Timestamp createdAt;
+    @CreationTimestamp
     private Timestamp updatedAt;
 
-    public Reply(Long boardId, String content, String replyer) {
-        this.boardId = boardId;
+    @ManyToOne
+    @JoinColumn(name = "replyer")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "boardId")
+    private Board board;
+
+    public void setReplyer(Member member) {
+        if (this.member != null) {
+            this.member.getReplies().remove(this);
+        }
+        this.member = member;
+        member.getReplies().add(this);   // 주인이 아니라서 저장 시 사용 안됨
+    }
+
+    public void setBoard(Board board) {
+        if (this.board != null) {
+            this.board.getReplies().remove(this);
+        }
+        this.board = board;
+        board.getReplies().add(this);   // 주인이 아니라서 저장 시 사용 안됨
+    }
+
+    public static Reply createReply(Member member, Board board, String content) {
+        Reply reply = new Reply();
+        reply.setReplyer(member);
+        reply.setBoard(board);
+        reply.content = content;
+        return reply;
+    }
+
+    public Reply(String content) {
+//        this.boardId = boardId;
         this.content = content;
-        this.replyer = replyer;
+//        this.replyer = replyer;
     }
 }
