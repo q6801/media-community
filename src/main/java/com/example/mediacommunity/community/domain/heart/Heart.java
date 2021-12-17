36dc0a5355271825568c5081e4a21d6ab79b3596
Lee SeungHun <q6801@naver.com>
2021-12-18 00:20:56 +0900

    feat: heart class에 jpa를 적용

--- src/main/java/com/example/mediacommunity/community/domain/heart/Heart.java
+++ src/main/java/com/example/mediacommunity/community/domain/heart/Heart.java
@@ -1,16 +1,48 @@
 package com.example.mediacommunity.community.domain.heart;
 
+import com.example.mediacommunity.community.domain.board.Board;
+import com.example.mediacommunity.community.domain.member.Member;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
+@Entity
+@NoArgsConstructor
 public class Heart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long boardId;
-    private String memberId;
+//    private Long boardId;
+//    private String memberId;
 
-    public Heart() {}
-    public Heart(Long boardId, String memberId) {
-        this.boardId = boardId;
-        this.memberId = memberId;
+    @ManyToOne
+    @JoinColumn(name = "memberId")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "boardId")
+    private Board board;
+
+    public void setMember(Member member) {
+        if (this.member != null) {
+            this.member.getHearts().remove(this);
+        }
+        this.member = member;
+        member.getHearts().add(this);   // 주인이 아니라서 저장 시 사용 안됨
     }
+
+    public void setBoard(Board board) {
+        if (this.board != null) {
+            this.board.getHearts().remove(this);
+        }
+        this.board = board;
+        board.getHearts().add(this);   // 주인이 아니라서 저장 시 사용 안됨
+    }
+
+//    public Heart(Long boardId, String memberId) {
+//        this.boardId = boardId;
+//        this.memberId = memberId;
+//    }
 }
