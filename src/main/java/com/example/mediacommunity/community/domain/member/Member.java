3d03fc2f472bdfff72e48add338a3f2a80062bd4
Lee SeungHun <q6801@naver.com>
2021-12-18 11:55:11 +0900

    feat : board, reply, heart와 양방향 연관관계 추가

--- src/main/java/com/example/mediacommunity/community/domain/member/Member.java
+++ src/main/java/com/example/mediacommunity/community/domain/member/Member.java
@@ -1,16 +1,19 @@
 package com.example.mediacommunity.community.domain.member;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import com.example.mediacommunity.community.domain.board.Board;
+import com.example.mediacommunity.community.domain.heart.Heart;
+import com.example.mediacommunity.community.domain.reply.Reply;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
-@AllArgsConstructor
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name="Members")
+@NoArgsConstructor
 public class Member {
     @Id
     private String loginId;
@@ -19,5 +22,20 @@ public class Member {
     private String provider;
     private String imageUrl;
 
-    public Member() {};
+    @OneToMany(mappedBy="member", fetch = FetchType.LAZY)
+    private List<Board> boards = new ArrayList<>();
+
+    @OneToMany(mappedBy="member", fetch = FetchType.LAZY)
+    private List<Reply> replies = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+    private List<Heart> hearts = new ArrayList<>();
+
+    public Member(String loginId, String password, String nickname, String provider, String imageUrl) {
+        this.loginId = loginId;
+        this.password = password;
+        this.nickname = nickname;
+        this.provider = provider;
+        this.imageUrl = imageUrl;
+    };
 }
