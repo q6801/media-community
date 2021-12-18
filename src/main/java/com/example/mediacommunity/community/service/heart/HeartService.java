963afe604c90b94aab54c4ada14d49bb008da61c
Lee SeungHun <q6801@naver.com>
2021-12-18 13:35:47 +0900

    fix : heart와 board, member는 양방향 연관되어서 각 값들이 필요하다.

--- src/main/java/com/example/mediacommunity/community/service/heart/HeartService.java
+++ src/main/java/com/example/mediacommunity/community/service/heart/HeartService.java
@@ -8,10 +8,10 @@ import java.util.Optional;
 public interface HeartService {
     Optional<Heart> findTheHeart(Long boardId, String memberId);
     /**
-     * @param heart
+     *
      * @return 좋아요 true, 안좋아요는 false
      */
-    Boolean toggleTheHeart(Heart heart);
+    Boolean toggleTheHeart(Long boardId, String MemberId);
     List<Heart> findLikingBoards(String memberId);
     List<Heart> findLikingMembers(Long boardId);
     Long cntHearts(Long boardId);
