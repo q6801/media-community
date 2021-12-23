fddafe256f345911cd12bcb643a7fa7a0d888a59
Lee SeungHun <q6801@naver.com>
2021-12-23 10:39:41 +0900

    feat : emailToken의 repository 작성

---
+++ src/main/java/com/example/mediacommunity/community/repository/EmailConfirmationTokenRepository.java
@@ -0,0 +1,27 @@
+package com.example.mediacommunity.community.repository;
+
+import com.example.mediacommunity.community.domain.EmailConfirmationToken;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.UUID;
+
+@Repository
+public class EmailConfirmationTokenRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public EmailConfirmationToken findById(UUID id) {
+        return em.find(EmailConfirmationToken.class, id);
+    }
+
+    public void save(EmailConfirmationToken emailToken) {
+        em.persist(emailToken);
+    }
+
+    public void delete(EmailConfirmationToken emailToken) {
+        em.remove(emailToken);
+    }
+}
