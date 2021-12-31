5c16616f26d584b93e3f07e465eaab16d9a3bd18
Lee SeungHun <q6801@naver.com>
2021-12-31 23:54:29 +0900

    feat : 웹소켓을 위한 설정 추가

---
+++ src/main/java/com/example/mediacommunity/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.example.mediacommunity.config;
+
+import com.example.mediacommunity.common.handler.ChatHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatHandler chatHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        System.out.println("fuckkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
+        registry
+                .addHandler(chatHandler, "/fuck")
+                .setAllowedOrigins("http://localhost:8080")
+//                .withSockJS()
+        ;
+    }
+}
