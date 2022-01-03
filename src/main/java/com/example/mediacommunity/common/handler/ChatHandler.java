ea697651712df2a97225d48f9043359ffaa5dd75
Lee SeungHun <q6801@naver.com>
2022-01-03 15:30:51 +0900

    feat: 받은 message를 objectMapper로 객체로 풀어냄

--- src/main/java/com/example/mediacommunity/common/handler/ChatHandler.java
+++ src/main/java/com/example/mediacommunity/common/handler/ChatHandler.java
@@ -1,5 +1,10 @@
 package com.example.mediacommunity.common.handler;
 
+import com.example.mediacommunity.community.domain.Message;
+import com.example.mediacommunity.community.domain.MsgRoom;
+import com.example.mediacommunity.community.service.MsgService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -12,34 +17,37 @@ import java.util.List;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class ChatHandler extends TextWebSocketHandler {
     private static List<WebSocketSession> list = new ArrayList<>();
+    private final MsgService msgService;
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
         log.info("payload : " + payload);
 
-        for(WebSocketSession sess: list) {
-            sess.sendMessage(message);
-        }
+        Message msg = objectMapper.readValue(payload, Message.class);
+        MsgRoom room = msgService.findById(msg.getRoomId());
+        room.handleActions(session, msg, msgService);
     }
 
-    /* Client가 접속 시 호출되는 메서드 */
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-
-        list.add(session);
-
-        log.info(session + " 클라이언트 접속");
-    }
-
-    /* Client가 접속 해제 시 호출되는 메서드드 */
-
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-
-        log.info(session + " 클라이언트 접속 해제");
-        list.remove(session);
-    }
+//    /* Client가 접속 시 호출되는 메서드 */
+//    @Override
+//    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+//
+//        list.add(session);
+//
+//        log.info(session + " 클라이언트 접속");
+//    }
+//
+//    /* Client가 접속 해제 시 호출되는 메서드드 */
+//
+//    @Override
+//    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+//
+//        log.info(session + " 클라이언트 접속 해제");
+//        list.remove(session);
+//    }
 }
