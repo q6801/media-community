723b66e3764e57432e5437bd0af61667158d0a0e
Lee SeungHun <q6801@naver.com>
2022-01-04 23:37:16 +0900

    feat: jpa 적용

--- src/main/java/com/example/mediacommunity/community/domain/MsgRoom.java
+++ src/main/java/com/example/mediacommunity/community/domain/MsgRoom.java
@@ -1,32 +1,49 @@
 package com.example.mediacommunity.community.domain;
 
 import com.example.mediacommunity.community.service.MsgService;
-import lombok.Builder;
-import lombok.ToString;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.web.socket.WebSocketSession;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
-@ToString
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class MsgRoom {
-    private String roomId;
-    private Set<WebSocketSession> sessions = new HashSet<>();
 
-    @Builder
-    public MsgRoom(String roomId) {
-        this.roomId = roomId;
-    }
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
 
-    public void handleActions(WebSocketSession session, Message message, MsgService msgService) {
-        if (message.getMessageType().equals(Message.Messagetype.ENTER)) {
-            sessions.add(session);
-            message.setMessage(message.getSender() + "님이 입장");
-        }
-        sendMessage(message, msgService);
-    }
+    @Column(nullable = false)
+    private String roomName;
 
-    public <T> void sendMessage(T message, MsgService messageService) {
-        sessions.parallelStream().forEach(session->messageService.sendMessage(session, message));
-    }
+//
+//    private Set<WebSocketSession> sessions = new HashSet<>();
+//
+//    @Builder
+//    public MsgRoom(String roomId) {
+//        this.roomId = roomId;
+//    }
+//
+//    public void handleActions(WebSocketSession session, Message message, MsgService msgService) {
+//        if (message.getMessageType().equals(Message.Messagetype.ENTER)) {
+//            sessions.add(session);
+//            message.setMessage(message.getSender() + "님이 입장");
+//        }
+//        sendMessage(message, msgService);
+//    }
+//
+//    public <T> void sendMessage(T message, MsgService messageService) {
+//        sessions.parallelStream().forEach(session->messageService.sendMessage(session, message));
+//    }
 }
