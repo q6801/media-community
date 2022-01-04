41693d6a3cb31dd320a45ed6a7fa997400c88fb3
Lee SeungHun <q6801@naver.com>
2022-01-04 23:52:03 +0900

    feat: stomp 적용

--- src/main/java/com/example/mediacommunity/config/WebSocketConfig.java
+++ src/main/java/com/example/mediacommunity/config/WebSocketConfig.java
@@ -1,25 +1,51 @@
 package com.example.mediacommunity.config;
 
-import com.example.mediacommunity.common.handler.ChatHandler;
+//import com.example.mediacommunity.common.handler.ChatHandler;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-
-    private final ChatHandler chatHandler;
-
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
-                .addHandler(chatHandler, "/ws/chat")
+                .addEndpoint("/ws/chat")
                 .setAllowedOrigins("http://localhost:8080")
 //                .withSockJS()
         ;
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                String user = accessor.getFirstNativeHeader("user");
+                System.out.println("user = " + user);
+                System.out.println("accessor = " + accessor.getUser());
+                return ChannelInterceptor.super.preSend(message, channel);
+            }
+        });
     }
 }
