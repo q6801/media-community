2e530cd12df2ffec39fcb978935829baa2d8dce3
Lee SeungHun <q6801@naver.com>
2022-01-04 23:39:20 +0900

    feat: jpa 적용

--- src/main/java/com/example/mediacommunity/community/service/MsgService.java
+++ src/main/java/com/example/mediacommunity/community/service/MsgService.java
@@ -1,53 +1,45 @@
 package com.example.mediacommunity.community.service;
 
 import com.example.mediacommunity.community.domain.MsgRoom;
+import com.example.mediacommunity.community.repository.MsgRoomRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.UUID;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MsgService {
     private final ObjectMapper objectMapper;
-    private Map<String, MsgRoom> msgRooms;
+    private final MsgRoomRepository msgRoomRepository;
 
-    @PostConstruct
-    private void init() {
-        msgRooms = new LinkedHashMap<>();
+    public MsgRoom createRoom(String name) {
+        MsgRoom msgRoom = MsgRoom.builder()
+                .roomName(name)
+                .build();
+        msgRoomRepository.saveMsgRoom(msgRoom);
+        return msgRoom;
+    }
+
+    public MsgRoom findById(UUID roomId) {
+        return msgRoomRepository.findByRoomId(roomId);
     }
 
     public List<MsgRoom> findAllRoom() {
-        return new ArrayList<>(msgRooms.values());
+        return msgRoomRepository.findAllRoom();
     }
 
-    public MsgRoom findById(String roomId) {
-        return msgRooms.get(roomId);
-    }
-
-    public MsgRoom createRoom(String name) {
-        // TODO : roomId == Summoner MatchId + blue/red Team code + Summoner Name
-        String roomId = name;
-        MsgRoom ms = MsgRoom.builder().roomId(roomId).build();
-        msgRooms.put(name, ms);
-        return ms;
-    }
-
-    public <T> void sendMessage(WebSocketSession session, T message) {
-        try {
-            session.sendMessage(new TextMessage(objectMapper.writeValueAsString(message)));
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
-        }
-    }
+//    public <T> void sendMessage(WebSocketSession session, T message) {
+//        try {
+//            session.sendMessage(new TextMessage(objectMapper.writeValueAsString(message)));
+//        } catch (IOException e) {
+//            log.error(e.getMessage(), e);
+//        }
+//    }
 }
