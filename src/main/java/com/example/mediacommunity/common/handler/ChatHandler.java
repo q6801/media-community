36f7ff7eafbda425e06983b783544ee446a7c947
Lee SeungHun <q6801@naver.com>
2022-01-05 10:45:45 +0900

    fix: stomp를 사용하면 더이상 웹소켓 handler를 사용할 필요가 없다.

--- src/main/java/com/example/mediacommunity/common/handler/ChatHandler.java
+++ src/main/java/com/example/mediacommunity/common/handler/ChatHandler.java
@@ -1,53 +1,53 @@
-package com.example.mediacommunity.common.handler;
-
-import com.example.mediacommunity.community.domain.Message;
-import com.example.mediacommunity.community.domain.MsgRoom;
-import com.example.mediacommunity.community.service.MsgService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class ChatHandler extends TextWebSocketHandler {
-    private static List<WebSocketSession> list = new ArrayList<>();
-    private final MsgService msgService;
-    private final ObjectMapper objectMapper;
-
-    @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        String payload = message.getPayload();
-        log.info("payload : " + payload);
-
-        Message msg = objectMapper.readValue(payload, Message.class);
-        MsgRoom room = msgService.findById(msg.getRoomId());
-        room.handleActions(session, msg, msgService);
-    }
-
-//    /* Client가 접속 시 호출되는 메서드 */
-//    @Override
-//    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+//package com.example.mediacommunity.common.handler;
 //
-//        list.add(session);
+//import com.example.mediacommunity.community.domain.Message;
+//import com.example.mediacommunity.community.domain.MsgRoom;
+//import com.example.mediacommunity.community.service.MsgService;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.socket.CloseStatus;
+//import org.springframework.web.socket.TextMessage;
+//import org.springframework.web.socket.WebSocketSession;
+//import org.springframework.web.socket.handler.TextWebSocketHandler;
 //
-//        log.info(session + " 클라이언트 접속");
-//    }
+//import java.util.ArrayList;
+//import java.util.List;
 //
-//    /* Client가 접속 해제 시 호출되는 메서드드 */
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class ChatHandler extends TextWebSocketHandler {
+//    private static List<WebSocketSession> list = new ArrayList<>();
+//    private final MsgService msgService;
+//    private final ObjectMapper objectMapper;
 //
 //    @Override
-//    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+//    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+//        String payload = message.getPayload();
+//        log.info("payload : " + payload);
 //
-//        log.info(session + " 클라이언트 접속 해제");
-//        list.remove(session);
+//        Message msg = objectMapper.readValue(payload, Message.class);
+//        MsgRoom room = msgService.findById(msg.getRoomId());
+//        room.handleActions(session, msg, msgService);
 //    }
-}
+//
+////    /* Client가 접속 시 호출되는 메서드 */
+////    @Override
+////    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+////
+////        list.add(session);
+////
+////        log.info(session + " 클라이언트 접속");
+////    }
+////
+////    /* Client가 접속 해제 시 호출되는 메서드드 */
+////
+////    @Override
+////    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+////
+////        log.info(session + " 클라이언트 접속 해제");
+////        list.remove(session);
+////    }
+//}
