0a36876332bfaa8e0d857cb70793cabfe8111942
Lee SeungHun <q6801@naver.com>
2022-01-05 10:44:35 +0900

    feat: email sending test

---
+++ src/test/java/com/example/mediacommunity/community/service/EmailSenderServiceTest.java
@@ -0,0 +1,22 @@
+package com.example.mediacommunity.community.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+
+@SpringBootTest
+
+class EmailSenderServiceTest {
+    @Autowired
+    private EmailSenderService emailSenderService;
+
+    @Test
+    void email() {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo("q6801@naver.com");
+        simpleMailMessage.setSubject("회원가입 이메일 인증");
+        simpleMailMessage.setText("http://localhost:8080");
+        emailSenderService.sendMail(simpleMailMessage);
+    }
+}
