0923c757a401c551b0086c3c8bb875910276662a
Lee SeungHun <q6801@naver.com>
2022-01-10 13:03:16 +0900

    feat: restcontroller를 사용하도록 변경

--- src/main/java/com/example/mediacommunity/community/controller/email/EmailController.java
+++ src/main/java/com/example/mediacommunity/community/controller/email/EmailController.java
@@ -4,15 +4,15 @@ import com.example.mediacommunity.community.service.EmailConfirmationTokenServic
 import com.example.mediacommunity.community.service.member.MemberService;
 import com.example.mediacommunity.security.userInfo.UserInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class EmailController {
 
@@ -20,15 +20,10 @@ public class EmailController {
     private final MemberService memberService;
 
     @PostMapping("/wait-email")
-    public String sendEmailToken(@AuthenticationPrincipal UserInfo userInfo, @RequestParam String email) {
+    public ResponseEntity<?> sendEmailToken(@AuthenticationPrincipal UserInfo userInfo, @RequestBody Map<String, String> email) {
         System.out.println("email = " + email);
-        emailTokenService.saveAndSendEmailToken(userInfo.getUsername(), email);
-        return "redirect:/wait-email";
-    }
-
-    @GetMapping("/wait-email")
-    public String waitEmail() {
-        return "email/waitEmail";
+        emailTokenService.saveAndSendEmailToken(userInfo.getUsername(), email.get("email"));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/confirm-email")
