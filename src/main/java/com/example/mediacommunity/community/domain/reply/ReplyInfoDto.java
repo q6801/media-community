a1bd3c22b57cf786be7772ace7fe2dbd72dd2c93
Lee SeungHun <q6801@naver.com>
2022-01-10 12:46:52 +0900

    feat : reply를 보여줄 때 사용하는 dto

---
+++ src/main/java/com/example/mediacommunity/community/domain/reply/ReplyInfoDto.java
@@ -0,0 +1,15 @@
+package com.example.mediacommunity.community.domain.reply;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
+@Getter
+@AllArgsConstructor
+public class ReplyInfoDto {
+    private String writer;
+    private String content;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+}
