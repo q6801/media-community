139c5d50b64a4c12732f547ae34d8b6044cf4a3e
Lee SeungHun <q6801@naver.com>
2022-01-10 12:59:31 +0900

    feat: 변경하는 member의 정보를 받는 dto

--- src/main/java/com/example/mediacommunity/community/domain/member/MemberEditDto.java
+++ src/main/java/com/example/mediacommunity/community/domain/member/MemberEditDto.java
@@ -3,12 +3,14 @@ package com.example.mediacommunity.community.domain.member;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MemberEditDto {
     private MultipartFile file;
 
