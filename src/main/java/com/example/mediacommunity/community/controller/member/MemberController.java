67667d6d5bb80860024e6440d04305192683b902
Lee SeungHun <q6801@naver.com>
2022-01-10 13:00:38 +0900

    feat: restcontroller를 사용하도록 변경

--- src/main/java/com/example/mediacommunity/community/controller/member/MemberController.java
+++ src/main/java/com/example/mediacommunity/community/controller/member/MemberController.java
@@ -2,66 +2,61 @@ package com.example.mediacommunity.community.controller.member;
 
 import com.example.mediacommunity.community.domain.member.Member;
 import com.example.mediacommunity.community.domain.member.MemberEditDto;
-import com.example.mediacommunity.community.domain.member.RoleType;
+import com.example.mediacommunity.community.domain.member.MemberInfoDto;
+import com.example.mediacommunity.community.domain.member.SignUpDto;
 import com.example.mediacommunity.community.service.member.MemberService;
 import com.example.mediacommunity.security.userInfo.UserInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 import java.io.IOException;
 import java.util.Optional;
 
 @Slf4j
-@Controller
-@RequestMapping("/member")
+@RestController
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
 
-    @GetMapping()
-    public String memberInfo(@AuthenticationPrincipal UserInfo userInfo, Model model) {
-        Member authUser = memberService.findMemberById(userInfo.getUsername());
-        model.addAttribute("member", authUser);
-        return "member/memberInfo";
+    @GetMapping("/memberInfo")
+    public MemberInfoDto member(@AuthenticationPrincipal UserInfo userInfo) {
+        Member member = memberService.findMemberById(userInfo.getUsername());
+        return new MemberInfoDto(member.getLoginId(), member.getEmail(),
+                member.getNickname(), member.getImageUrl());
     }
 
-    @GetMapping("/edit")
-    public String editMemberForm(@AuthenticationPrincipal UserInfo userInfo, Model model) {
-        Member authUser = memberService.findMemberById(userInfo.getUsername());
-        model.addAttribute("imageUrl", authUser.getImageUrl());
-        model.addAttribute("memberEditDto", new MemberEditDto(null, authUser.getNickname()));
-        model.addAttribute("role", authUser.getRoleType().getCode());
-        return "member/memberEdit";
-    }
-
-    @PostMapping("/edit")
-    public String editMemberInfo(@AuthenticationPrincipal UserInfo userInfo, @Valid @ModelAttribute MemberEditDto memberEditDto,
-                                 BindingResult bindingResult, HttpSession session, Model model) throws ServletException, IOException {
-
+    @PutMapping("/member")
+    public String editMemberInfo(@ModelAttribute MemberEditDto memberEditDto,
+                                 @AuthenticationPrincipal UserInfo userInfo) throws IOException {
         String role = userInfo.getRole();
         System.out.println("role = " + role);
-        model.addAttribute("role", role);
-        if (bindingResult.hasErrors()) {
-            return "member/memberEdit";
-        }
+
         Optional<String> imageUrl = memberService.updateProfile(userInfo.getUsername(), memberEditDto);
-        if (imageUrl.isEmpty()) {
-            bindingResult.reject("nicknameDuplicated");
-            return "member/memberEdit";
-        } else {
-            model.addAttribute("imageUrl", imageUrl.get());
-        }
+        imageUrl.orElseThrow(() -> new RuntimeException());
         return "redirect:/";
     }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signUp(@RequestBody SignUpDto signUpDto) {
+        if (!memberService.encodeAndSave(signUpDto)) {
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+        throw new RuntimeException();
+    }
+
+    @DeleteMapping("/member")
+    public ResponseEntity<?> signout(@AuthenticationPrincipal UserInfo userInfo, HttpSession session) {
+        if (session != null) {
+            session.invalidate();
+        }
+        memberService.signOut(userInfo.getUsername());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
 }
