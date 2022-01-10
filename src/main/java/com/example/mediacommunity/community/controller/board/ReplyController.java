747bc1f1c5ad47df68714447e69673f5e622d91d
Lee SeungHun <q6801@naver.com>
2022-01-10 12:47:22 +0900

    refactor : reply를 restcontroller로 변경

--- src/main/java/com/example/mediacommunity/community/controller/board/ReplyController.java
+++ src/main/java/com/example/mediacommunity/community/controller/board/ReplyController.java
@@ -1,25 +1,35 @@
 package com.example.mediacommunity.community.controller.board;
 
-import com.example.mediacommunity.community.domain.reply.ReplyDto;
+import com.example.mediacommunity.community.domain.reply.Reply;
+import com.example.mediacommunity.community.domain.reply.ReplyInfoDto;
+import com.example.mediacommunity.community.domain.reply.ReplyInputDto;
 import com.example.mediacommunity.community.service.reply.ReplyService;
 import com.example.mediacommunity.security.userInfo.UserInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
-@RequestMapping("/reply")
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
 @RequiredArgsConstructor
 public class ReplyController {
     private final ReplyService replyService;
 
-    @PostMapping("/{boardId}")
-    public String reply(@ModelAttribute ReplyDto replyInfo, @RequestParam int page, RedirectAttributes redirectAttributes,
-                        @PathVariable Long boardId, @AuthenticationPrincipal UserInfo userInfo) {
-        replyService.reply(boardId, userInfo.getUsername(), replyInfo.getContent());
-        redirectAttributes.addAttribute("page", page);
-        return "redirect:/boards/{boardId}";
+    @PostMapping("reply/{boardId}")
+    public ReplyInfoDto reply(@RequestBody ReplyInputDto replyDto, @PathVariable Long boardId,
+                                @AuthenticationPrincipal UserInfo userInfo) {
+        Reply reply = replyService.reply(boardId, userInfo.getUsername(), replyDto.getContent());
+        return reply.convertReplyToReplyInfoDto();
     }
+
+    @GetMapping("boardInfo/{boardIdx}/replies")
+    public List<ReplyInfoDto> replies(@PathVariable long boardIdx) {
+        List<Reply> allReplies = replyService.findAllReplies(boardIdx);
+        return allReplies.stream().map(reply -> reply.convertReplyToReplyInfoDto())
+                .collect(Collectors.toList());
+    }
+
 }
