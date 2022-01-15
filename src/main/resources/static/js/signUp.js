595009d9e19fe2c99a706365290be338455daddf
Lee SeungHun <q6801@naver.com>
2022-01-15 14:40:13 +0900

    fix: dev 상태로 변경

--- src/main/resources/static/js/signUp.js
+++ src/main/resources/static/js/signUp.js
@@ -12,5 +12,13 @@ sign_up_btn.addEventListener('click', function() {
     })
     .then(function(res) {
         window.location.replace('/')
+    }).catch(function(err) {
+        console.log('err : ' +  err.response.data.errorCode)
+        console.log('err : ' +  err.response.data.errorMessage)
+
+        pw.value = ''
+
+        let error_dom = document.querySelector('#error')
+        error_dom.innerText = err.response.data.errorMessage
     })
 })
