79ea16f1eed37a48e600d8773bfc28cc805fa7b1
Lee SeungHun <q6801@naver.com>
2022-01-18 23:58:34 +0900

    feat: id가 아닌 member로 mail token을 찾는다.

--- src/main/java/com/example/mediacommunity/community/repository/EmailConfirmationTokenRepository.java
+++ src/main/java/com/example/mediacommunity/community/repository/EmailConfirmationTokenRepository.java
@@ -1,10 +1,12 @@
 package com.example.mediacommunity.community.repository;
 
 import com.example.mediacommunity.community.domain.email.EmailConfirmationToken;
+import com.example.mediacommunity.community.domain.member.Member;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -13,8 +15,8 @@ public class EmailConfirmationTokenRepository {
     @PersistenceContext
     private EntityManager em;
 
-    public EmailConfirmationToken findById(UUID id) {
-        return em.find(EmailConfirmationToken.class, id);
+    public Optional<EmailConfirmationToken> findRecentMail(Member member) {
+        return member.getEmailTokens().stream().findFirst();
     }
 
     public void save(EmailConfirmationToken emailToken) {
