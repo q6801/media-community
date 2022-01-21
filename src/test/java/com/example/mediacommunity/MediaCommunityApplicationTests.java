a30c42913fd9b9677cb02997e52a6fbf588ad248
Lee SeungHun <q6801@naver.com>
2022-01-21 15:09:44 +0900

    fix: WebAppConfiguration 어노테이션을 추가한 것과 같은 효과를냄
    WebSocketConfig에서 ServletServerContainerFactoryBean을 사용하면 오류남. 해결하려면 WebAppConfiguration을 추가해야됨. 그게 나중에 SpringBootTest의 속성으로 포함됨

--- src/test/java/com/example/mediacommunity/MediaCommunityApplicationTests.java
+++ src/test/java/com/example/mediacommunity/MediaCommunityApplicationTests.java
@@ -2,8 +2,9 @@ package com.example.mediacommunity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
 class MediaCommunityApplicationTests {
 
 	@Test
