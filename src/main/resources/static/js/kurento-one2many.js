baab083ff5f4e2fcb5f3fb90edcff9783c70a286
Lee SeungHun <q6801@naver.com>
2022-01-21 12:00:01 +0900

    feat: 동영상 streaming html, js 추가

---
+++ src/main/resources/static/js/kurento-one2many.js
@@ -0,0 +1,180 @@
+const ws = new WebSocket('wss://' + location.host + '/helloworld');
+var video;
+var webRtcPeer;
+let presenter_dom = document.querySelector('#presenter')
+let viewer_dom = document.querySelector('#viewer')
+let waiting = document.querySelector('#waiting')
+
+
+window.onload = function() {
+	video = document.getElementById('localVideo');
+}
+
+window.onbeforeunload = function() {
+	ws.close();
+}
+
+presenter_dom.addEventListener('click', function() {
+    presenter()
+})
+viewer_dom.addEventListener('click', function() {
+    viewer()
+})
+
+
+ws.onmessage = function(message) {
+	var parsedMessage = JSON.parse(message.data);
+	console.info('Received message: ' + message.data);
+
+	switch (parsedMessage.id) {
+	case 'presenterResponse':
+		presenterResponse(parsedMessage);
+		break;
+	case 'viewerResponse':
+		viewerResponse(parsedMessage);
+		break;
+	case 'iceCandidate':
+		webRtcPeer.addIceCandidate(parsedMessage.candidate, function(error) {
+			if (error)
+				return console.error('Error adding candidate: ' + error);
+		});
+		break;
+	case 'stopCommunication':
+		dispose();
+		break;
+	default:
+		console.error('Unrecognized message', parsedMessage);
+	}
+}
+
+function presenterResponse(message) {
+	if (message.response != 'accepted') {
+		var errorMsg = message.message ? message.message : 'Unknow error';
+		console.info('Call not accepted for the following reason: ' + errorMsg);
+		dispose();
+	} else {
+		webRtcPeer.processAnswer(message.sdpAnswer, function(error) {
+			if (error)
+				return console.error(error);
+		});
+	}
+}
+
+function viewerResponse(message) {
+	if (message.response != 'accepted') {
+		var errorMsg = message.message ? message.message : 'Unknow error';
+		console.info('Call not accepted for the following reason: ' + errorMsg);
+		dispose();
+	} else {
+		webRtcPeer.processAnswer(message.sdpAnswer, function(error) {
+			if (error)
+				return console.error(error);
+		});
+	}
+}
+
+function presenter() {
+	if (!webRtcPeer) {
+		var options = {
+			localVideo : video,
+			onicecandidate : onIceCandidate
+		}
+		webRtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerSendonly(options,
+				function(error) {
+					if (error) {
+						return console.error(error);
+					}
+					webRtcPeer.generateOffer(onOfferPresenter);
+				});
+        console.log('wrp')
+        console.log(webRtcPeer)
+	}
+}
+
+function onOfferPresenter(error, offerSdp) {
+	if (error)
+		return console.error('Error generating the offer');
+	console.info('Invoking SDP offer callback function ' + location.host);
+	var message = {
+		id : 'presenter',
+		sdpOffer : offerSdp
+	}
+	sendMessage(message);
+}
+
+function viewer() {
+	if (!webRtcPeer) {
+//		showSpinner(video);
+        videoWaiting(video);
+
+		var options = {
+			remoteVideo : video,
+			onicecandidate : onIceCandidate
+		}
+		webRtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options,
+				function(error) {
+					if (error) {
+						return console.error(error);
+					}
+					this.generateOffer(onOfferViewer);
+				});
+	}
+}
+
+function onOfferViewer(error, offerSdp) {
+	if (error)
+		return console.error('Error generating the offer');
+	console.info('Invoking SDP offer callback function ' + location.host);
+	var message = {
+		id : 'viewer',
+		sdpOffer : offerSdp
+	}
+	sendMessage(message);
+}
+
+function onIceCandidate(candidate) {
+	console.log("Local candidate" + JSON.stringify(candidate));
+
+	var message = {
+		id : 'onIceCandidate',
+		candidate : candidate
+	};
+	sendMessage(message);
+}
+
+function stop() {
+	var message = {
+		id : 'stop'
+	}
+	sendMessage(message);
+	dispose();
+}
+
+function dispose() {
+	if (webRtcPeer) {
+		webRtcPeer.dispose();
+		webRtcPeer = null;
+	}
+	hideVideoWaiting();
+}
+
+function sendMessage(message) {
+	var jsonMessage = JSON.stringify(message);
+	console.log('Sending message: ' + jsonMessage);
+	ws.send(jsonMessage);
+}
+
+function videoWaiting() {
+    let div = document.createElement('div');
+    div.setAttribute('id', 'loading')
+    div.innerText = 'video 로딩중 ~'
+    waiting.appendChild(div)
+}
+
+function hideVideoWaiting() {
+    console.log('hide waiting')
+    let div = document.querySelector('#loading')
+    div.parentNode.removeChild(div)
+}
+
+
