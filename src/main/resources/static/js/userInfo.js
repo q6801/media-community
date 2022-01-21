d7a10eb7599b4087ac579fe9dddc4e6aaf4c0884
Lee SeungHun <q6801@naver.com>
2022-01-21 12:02:09 +0900

    refactor: email 글자 추가

--- src/main/resources/static/js/userInfo.js
+++ src/main/resources/static/js/userInfo.js
@@ -33,7 +33,7 @@ axios.get('/memberInfo')
         if (memberInfo.email !== null) {
             console.log(memberInfo.email)
             let card_text2 = document.createElement('p')
-            card_text2.innerText = memberInfo.email
+            card_text2.innerText = 'email : ' + memberInfo.email
             card_body.appendChild(card_text2)
         } else {
             let card_text2 = document.createElement('p')
@@ -48,10 +48,10 @@ axios.get('/memberInfo')
             card_body.appendChild(btn)
 
             btn.addEventListener('click', function() {
-                axios.post('/wait-email', {
+                axios.post('/email', {
                     email: email_input.value
                 }).then(function(res) {
-                    window.location.replace('/wait-email')
+                    window.location.replace('/confirm-email')
                 })
             })
         }
