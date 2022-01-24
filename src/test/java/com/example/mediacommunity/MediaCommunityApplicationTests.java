1560a7c22a35c762e88066b4cfdf33deac6e1e8d
Lee SeungHun <q6801@naver.com>
2022-01-24 10:46:54 +0900

    fix: springBootTest 다시 재개

--- src/test/java/com/example/mediacommunity/MediaCommunityApplicationTests.java
+++ src/test/java/com/example/mediacommunity/MediaCommunityApplicationTests.java
@@ -1,14 +1,14 @@
-//package com.example.mediacommunity;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//
-//@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
-//class MediaCommunityApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.example.mediacommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
+class MediaCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
