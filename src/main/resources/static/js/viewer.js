f2e0d1e1a16956b083ae61c1a78bbf50135e649e
Lee SeungHun <q6801@naver.com>
2022-01-28 22:29:04 +0900

    feat: streaming viewer html, js 추가

---
+++ src/main/resources/static/js/viewer.js
@@ -0,0 +1,147 @@
+//const ws = new WebSocket('wss://' + location.host + '/helloworld');
+var video;
+var webRtcPeer;
+let viewer_dom = document.querySelector('#viewer')
+let waiting = document.querySelector('#waiting')
+
+
+window.onload = function() {
+	video = document.getElementById('localVideo');
+}
+
+window.onbeforeunload = function() {
+	ws.close();
+}
+
+viewer_dom.addEventListener('click', function() {
+    viewer()
+})
+
+let webSocket = new WebSocket('wss://' + location.host + '/ws/stream');
+let client = Stomp.over(webSocket);
+
+client.connect({}, function() {
+//    client.subscribe("/sub/message/presenterResponse", function(message) {
+//            let parsedMessage = JSON.parse(message.body);
+//            presenterResponse(parsedMessage);
+//        })
+    client.subscribe("/sub/message/viewerResponse", function(message) {
+        let parsedMessage = JSON.parse(message.body);
+        viewerResponse(parsedMessage);
+    })
+    client.subscribe("/sub/message/iceCandidate/v", function(message) {
+        let parsedMessage = JSON.parse(message.body);
+        webRtcPeer.addIceCandidate(parsedMessage.candidate, function(error) {
+            if (error) return console.error('Error adding candidate: ' + error);
+        });
+    })
+    client.subscribe("/sub/message/stopCommunication", function(message) {
+        let parsedMessage = JSON.parse(message.body);
+        dispose();
+    })
+})
+
+function presenterResponse(message) {
+	if (message.response != 'accepted') {
+		var errorMsg = message.message ? message.message : 'Unknown error';
+		console.info('Call not accepted for the following reason: ' + errorMsg);
+		dispose();
+	} else {
+		webRtcPeer.processAnswer(message.sdpAnswer, function(error) {
+			if (error)
+				return console.error(error);
+		});
+	}
+}
+
+function viewerResponse(message) {
+	if (message.response != 'accepted') {
+		var errorMsg = message.message ? message.message : 'Unknow error';
+		console.info('Call not accepted for the following reason: ' + errorMsg);
+		dispose();
+	} else {
+		webRtcPeer.processAnswer(message.sdpAnswer, function(error) {
+			if (error)
+				return console.error(error);
+		});
+	}
+}
+
+
+function viewer() {
+	if (!webRtcPeer) {
+//		showSpinner(video);
+        videoWaiting(video);
+
+		var options = {
+			remoteVideo : video,
+			onicecandidate : onIceCandidate
+		}
+		webRtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options,
+				function(error) {
+					if (error) {
+						return console.error(error);
+					}
+					this.generateOffer(onOfferViewer);
+				});
+	}
+}
+
+function onOfferViewer(error, offerSdp) {
+	if (error)
+		return console.error('Error generating the offer');
+	console.info('Invoking SDP offer callback function ' + location.host);
+	var message = {
+		id : 'viewer',
+		sdpOffer : offerSdp
+	}
+	sendMessage(message);
+}
+
+function onIceCandidate(candidate) {
+//	console.log("Local candidate" + JSON.stringify(candidate));
+
+	var message = {
+		id : 'onIceCandidate',
+		candidate : candidate
+	};
+	sendMessage(message);
+}
+
+function stop() {
+	var message = {
+		id : 'stop'
+	}
+	sendMessage(message);
+	dispose();
+}
+
+function dispose() {
+	if (webRtcPeer) {
+		webRtcPeer.dispose();
+		webRtcPeer = null;
+	}
+	hideVideoWaiting();
+}
+
+function sendMessage(message) {
+	var jsonMessage = JSON.stringify(message);
+	client.send("/pub/streaming/" + message.id, {}, jsonMessage);
+
+//	ws.send(jsonMessage);
+}
+
+function videoWaiting() {
+    let div = document.createElement('div');
+    div.setAttribute('id', 'loading')
+    div.innerText = 'video 로딩중 ~'
+    waiting.appendChild(div)
+}
+
+function hideVideoWaiting() {
+    console.log('hide waiting')
+    let div = document.querySelector('#loading')
+    div.parentNode.removeChild(div)
+}
+
+
