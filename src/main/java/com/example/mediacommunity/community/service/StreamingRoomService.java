7e185bccfd2f9bd7695be93c215d1d57d5a310d2
Lee SeungHun <q6801@naver.com>
2022-01-31 23:46:20 +0900

    feat: streamingRoom service

---
+++ src/main/java/com/example/mediacommunity/community/service/StreamingRoomService.java
@@ -0,0 +1,46 @@
+package com.example.mediacommunity.community.service;
+
+import com.example.mediacommunity.community.domain.chat.StreamingRoom;
+import com.example.mediacommunity.community.repository.StreamingRoomRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class StreamingRoomService {
+    private final StreamingRoomRepository streamingRoomRepository;
+
+    public StreamingRoom createRoom(String roomName, String memberId) {
+        StreamingRoom streamingRoom = StreamingRoom.builder()
+                .roomName(roomName)
+                .presenter(memberId)
+                .build();
+        streamingRoomRepository.saveRoom(streamingRoom);
+        return streamingRoom;
+    }
+
+    public StreamingRoom findById(UUID roomId) {
+        return streamingRoomRepository.findByRoomId(roomId);
+    }
+
+    public Optional<StreamingRoom> findByPresenter(String username) {
+        return streamingRoomRepository.findByPresenter(username);
+    }
+
+    public List<StreamingRoom> findAllRoom() {
+        return streamingRoomRepository.findAllRoom();
+    }
+
+    public void deleteRoom(String username) {
+        findByPresenter(username)
+                .ifPresent((room) -> streamingRoomRepository.deleteRoom(room));
+    }
+}
