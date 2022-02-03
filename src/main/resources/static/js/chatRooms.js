cc7789954e1ac673900caadacab3fc37c90b722b
Lee SeungHun <q6801@naver.com>
2022-02-03 21:16:57 +0900

    feat: 기존의 msg를 streaming 데이터로 변경

--- src/main/resources/static/js/chatRooms.js
+++ src/main/resources/static/js/chatRooms.js
@@ -1,4 +1,4 @@
-axios.get('/chat/rooms')
+axios.get('/streaming-rooms')
     .then(function(res) {
         console.log(res.data)
 
@@ -17,9 +17,9 @@ axios.get('/chat/rooms')
 
         // 테이블 헤드 제작
         let tr = document.createElement('tr')
-        let td = new Array(1);
-        let table_head_innertext = ['roomName']
-        for(let i=0; i<1; i++) {
+        let td = new Array(2);
+        let table_head_innertext = ['roomName', 'streamer']
+        for(let i=0; i<td.length; i++) {
             td[i] = document.createElement('td')
             td[i].innerText = table_head_innertext[i]
             tr.appendChild(td[i])
@@ -28,15 +28,16 @@ axios.get('/chat/rooms')
 
         // 테이블 바디 제작
         for(idx in msgRooms) {
-            let msgRoom = msgRooms[idx]
+            let streamingRoom = msgRooms[idx]
             let tr = document.createElement('tr')
-            tr.setAttribute('onClick', `location.href="/chatRoom/${msgRoom.id}"`)
-            let td = new Array(4);
-            for(let i=0; i<1; i++) {
+            tr.setAttribute('onClick', `location.href="/${streamingRoom.presenter}"`)
+            let td = new Array(2);
+            for(let i=0; i<td.length; i++) {
                 td[i] = document.createElement('td')
                 tr.appendChild(td[i])
             }
-            td[0].innerText = msgRoom.roomName
+            td[0].innerText = streamingRoom.roomName
+            td[1].innerText = streamingRoom.presenter
             tbody.appendChild(tr)
         }
                   
