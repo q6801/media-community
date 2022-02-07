521165c8d8241a94e890c03ebebff0e4f89461e9
Lee SeungHun <q6801@naver.com>
2022-02-07 21:30:25 +0900

    feat: objectMapper를 쓰기 위해 no args contructor 추가

--- src/main/java/com/example/mediacommunity/community/domain/member/SignUpDto.java
+++ src/main/java/com/example/mediacommunity/community/domain/member/SignUpDto.java
@@ -2,10 +2,12 @@ package com.example.mediacommunity.community.domain.member;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class SignUpDto {
     @NotBlank
@@ -16,6 +18,4 @@ public class SignUpDto {
 
     @NotBlank
     private String nickname;
-
-    public SignUpDto() {}
 }
