13c202bdeee38b208fcbd51c28a2df7c4be6b9af
Lee SeungHun <q6801@naver.com>
2022-02-13 00:23:10 +0900

    fix: findByPresenter의 파라미터에 roomType 추가

--- src/main/java/com/example/mediacommunity/community/service/RoomServiceImpl.java
+++ src/main/java/com/example/mediacommunity/community/service/RoomServiceImpl.java
@@ -33,8 +33,8 @@ public class RoomServiceImpl {
         return roomRepository.findByRoomId(roomId);
     }
 
-    public Optional<Room> findByPresenter(String username) {
-        return roomRepository.findByPresenter(username);
+    public Optional<Room> findByPresenter(String username, RoomType roomType) {
+        return roomRepository.findByPresenter(username, roomType);
     }
 
     public List<Room> findRoomsByType(RoomType roomType) {
