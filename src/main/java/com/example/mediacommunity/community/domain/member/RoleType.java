e8a275288271203fabd471341fbe147414503e82
Lee SeungHun <q6801@naver.com>
2022-02-14 13:49:55 +0900

    feat: streamer role type 추가

--- src/main/java/com/example/mediacommunity/community/domain/member/RoleType.java
+++ src/main/java/com/example/mediacommunity/community/domain/member/RoleType.java
@@ -10,7 +10,9 @@ import java.util.Arrays;
 public enum RoleType {
     USER("ROLE_USER", "일반 사용자 권한"),
     ADMIN("ROLE_ADMIN", "관리자 권한"),
-    GUEST("ROLE_GUEST", "게스트 권한");
+    GUEST("ROLE_GUEST", "게스트 권한"),
+    STREAMER("ROLE_STREAMER", "스트리머 권한")
+    ;
 
     private final String code;
     private final String displayName;
