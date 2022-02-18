96b0487034ce8ca9b0500b1bdd894797dd5068e5
Lee SeungHun <q6801@naver.com>
2022-02-18 23:43:45 +0900

    refactor : 작성된 날짜를 글을 작성하고 지난 시간을 표기로 변경

--- src/main/resources/static/js/board/boards.js
+++ src/main/resources/static/js/board/boards.js
@@ -53,7 +53,8 @@ axios.get('/boards/' + category + '?page=' + page)
         td[0].innerText = board.title
         td[1].innerText = board.writer
         td[2].innerText = board.viewCnt
-        td[3].innerText = board.createdAt
+//        td[3].innerText = board.createdAt
+        td[3].innerText = timeForToday(new Date(board.createdAt))
         tbody.appendChild(tr)
     }
 
@@ -108,3 +109,26 @@ axios.get('/board-category')
         categoryDom.appendChild(li)
     }
 })
+
+function timeForToday(value) {
+        const today = new Date();
+        const timeValue = new Date(value);
+
+        const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
+        if (betweenTime < 1) return '방금전';
+        if (betweenTime < 60) {
+            return `${betweenTime}분전`;
+        }
+
+        const betweenTimeHour = Math.floor(betweenTime / 60);
+        if (betweenTimeHour < 24) {
+            return `${betweenTimeHour}시간전`;
+        }
+
+        const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+        if (betweenTimeDay < 365) {
+            return `${betweenTimeDay}일전`;
+        }
+
+        return `${Math.floor(betweenTimeDay / 365)}년전`;
+ }
