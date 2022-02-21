de603105c24c20bf472dbe8c00aa3c6125c695e1
Lee SeungHun <q6801@naver.com>
2022-02-21 22:24:28 +0900

    refactor: springboottest의 webEnvironment를 random_port로 변경

--- src/test/java/com/example/mediacommunity/community/controller/IntegrationAPITest.java
+++ src/test/java/com/example/mediacommunity/community/controller/IntegrationAPITest.java
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
-@SpringBootTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class IntegrationAPITest {
     @Autowired
