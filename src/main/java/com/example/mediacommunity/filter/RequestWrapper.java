bd1ce4799bcd853e3a94a910dc1fadc56aacebd8
Lee SeungHun <q6801@naver.com>
2022-02-23 01:09:50 +0900

    refactor: 코드 리팩토링

--- src/main/java/com/example/mediacommunity/filter/RequestWrapper.java
+++ src/main/java/com/example/mediacommunity/filter/RequestWrapper.java
@@ -1,5 +1,6 @@
 package com.example.mediacommunity.filter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
@@ -24,39 +25,44 @@ public class RequestWrapper extends HttpServletRequestWrapper {
 
     public RequestWrapper(HttpServletRequest request) throws IOException {
         super(request);
-
+        ObjectMapper objectMapper = new ObjectMapper();
         try {
-            Safelist safelist = Safelist.basic();
-            safelist.addAttributes(":all", "class");
-
             InputStream inputStream = request.getInputStream();
             String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            Map<String, String> map;
-            if (messageBody.isBlank()) {
-                map = new HashMap<>();
-            } else {
-                map = objectMapper.readValue(messageBody, Map.class);
-            }
-            map.forEach((key, value) -> System.out.println(key +" : " + value));
-
-            for(String key : map.keySet()) {
-                String value = map.get(key);
-                if (key.equals("content")) {
-                    String cleanBody = Jsoup.clean(value, "", safelist, new Document.OutputSettings().prettyPrint(false));
-                    String quotReplacedBody = cleanBody.replaceAll("\"\\\\&quot;", "\'");
-                    quotReplacedBody = quotReplacedBody.replaceAll("\\\\&quot;\"", "\'");
-                    map.put(key, quotReplacedBody);
-                } else {
-                    String htmlEscapedValue = HtmlUtils.htmlEscape(value);
-                    map.put(key, htmlEscapedValue);
-                }
-            }
-
-            newData = objectMapper.writeValueAsBytes(map);
+            Map<String, String> requestMessageMap = changeMessageBodyToMap(messageBody, objectMapper);
+            xssDefence(requestMessageMap);
+            newData = objectMapper.writeValueAsBytes(requestMessageMap);
         } catch (IOException e) {
             throw e;
+        }
+    }
+
+    private Map<String, String> changeMessageBodyToMap(String messageBody, ObjectMapper objectMapper) throws JsonProcessingException {
+        Map<String, String> map;
+        if (messageBody.isBlank()) {
+            map = new HashMap<>();
+        } else {
+            map = objectMapper.readValue(messageBody, Map.class);
+        }
+        return map;
+    }
+
+    private void xssDefence(Map<String, String> map) {
+        Safelist safelist = Safelist.basic();
+        safelist.addAttributes(":all", "class");
+
+        for(String key : map.keySet()) {
+            String value = map.get(key);
+            if (key.equals("content")) {
+                String cleanBody = Jsoup.clean(value, "", safelist, new Document.OutputSettings().prettyPrint(false));
+                String quotReplacedBody = cleanBody.replaceAll("\"\\\\&quot;", "\'");
+                quotReplacedBody = quotReplacedBody.replaceAll("\\\\&quot;\"", "\'");
+                map.put(key, quotReplacedBody);
+            } else {
+                String htmlEscapedValue = HtmlUtils.htmlEscape(value);
+                map.put(key, htmlEscapedValue);
+            }
         }
     }
 
