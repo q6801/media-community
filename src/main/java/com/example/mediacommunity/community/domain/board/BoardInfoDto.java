7880e63e47d8525e216402d0ad1e94c7e96e136f
Lee SeungHun <q6801@naver.com>
2022-04-05 18:12:57 +0900

    feat: boardInfoDto에 replyCnt, heartCnt 필드 추가

--- src/main/java/com/example/mediacommunity/community/domain/board/BoardInfoDto.java
+++ src/main/java/com/example/mediacommunity/community/domain/board/BoardInfoDto.java
@@ -2,20 +2,25 @@ package com.example.mediacommunity.community.domain.board;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 
 @AllArgsConstructor
 @Getter
+@Setter
+@NoArgsConstructor
 public class BoardInfoDto {
     private Long id;
     private String content;
     private Timestamp createdAt;
     private Timestamp updatedAt;
     private int viewCnt;
+    private int replyCnt;
+    private int heartCnt;
     private String title;
     private String writer;
-    private int repliesCnt;
     private boolean anonymous;
     private String category;
 }
