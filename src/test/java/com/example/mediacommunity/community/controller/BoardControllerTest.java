e30d77acbf910471ab000ae0bcdc63a5bd3c8d33
Lee SeungHun <q6801@naver.com>
2022-04-11 21:52:33 +0900

    test: board controller api test

---
+++ src/test/java/com/example/mediacommunity/community/controller/BoardControllerTest.java
@@ -0,0 +1,113 @@
+package com.example.mediacommunity.community.controller;
+
+import com.example.mediacommunity.community.controller.board.BoardController;
+import com.example.mediacommunity.community.domain.board.BoardRequestDto;
+import com.example.mediacommunity.community.domain.member.Member;
+import com.example.mediacommunity.community.service.board.BoardCategoryService;
+import com.example.mediacommunity.community.service.board.BoardService;
+import com.example.mediacommunity.community.service.member.MemberService;
+import com.example.mediacommunity.security.WithMockCustomUser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BoardControllerTest extends BeforeTest{
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private BoardCategoryService boardCategoryService;
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private BoardService boardService;
+
+    private String categoryName = "testCategory";
+    private long createdBoardId;
+
+    @BeforeEach
+    public void beforeTest() throws Exception {
+        createCategory(boardCategoryService, categoryName);
+        Member member = saveMember(memberService);
+        createdBoardId = createBoard(boardService, categoryName, member);
+    }
+
+    @Test
+    @DisplayName("board category 조회 성공 테스트")
+    public void boards() throws Exception {
+        ResultActions result = mockMvc.perform(get("/api/boards/" + categoryName)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(BoardController.class))
+                .andExpect(handler().methodName("boards"));
+    }
+
+    @Test
+    @WithMockCustomUser
+    @DisplayName("board 조회 성공 테스트")
+    public void board() throws Exception {
+        ResultActions result = mockMvc.perform(
+                get("/api/board/" + createdBoardId)
+                .accept(MediaType.APPLICATION_JSON));
+        result.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(BoardController.class))
+                .andExpect(handler().methodName("board"));
+
+    }
+
+    @Test
+    @WithMockCustomUser
+    @DisplayName("board 작성 성공 테스트")
+    public void createBoardWithTest() throws Exception {
+        BoardRequestDto boardAddingDto =
+                new BoardRequestDto("title", "content", categoryName, false);
+        ResultActions result =  mockMvc.perform(
+                post("/api/board")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(boardAddingDto))
+        );
+        result.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(handler().handlerType(BoardController.class))
+                .andExpect(handler().methodName("addBoard"));
+    }
+
+    @Test
+    @WithMockCustomUser
+    @DisplayName("board 수정, 삭제 성공 테스트")
+    public void addAndEditAndDeleteBoard() throws Exception {
+        BoardRequestDto boardEditingDto =
+                new BoardRequestDto("newTitle", "newContent", categoryName, false);
+
+        mockMvc.perform(
+                put("/api/board/" + createdBoardId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(boardEditingDto))
+        ).andExpect(status().isCreated());
+
+        mockMvc.perform(
+                delete("/api/board/" + createdBoardId)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNoContent());
+    }
+}
