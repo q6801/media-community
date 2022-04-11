7171f07957a4ad6ab947911ebfcc461d8b913850
Lee SeungHun <q6801@naver.com>
2022-04-11 21:51:48 +0900

    feat: api test에 필요한 db 생성을 위한 class

---
+++ src/test/java/com/example/mediacommunity/community/controller/BeforeTest.java
@@ -0,0 +1,37 @@
+package com.example.mediacommunity.community.controller;
+
+import com.example.mediacommunity.community.domain.board.Board;
+import com.example.mediacommunity.community.domain.board.BoardRequestDto;
+import com.example.mediacommunity.community.domain.category.BoardCategory;
+import com.example.mediacommunity.community.domain.member.Member;
+import com.example.mediacommunity.community.domain.member.SignUpDto;
+import com.example.mediacommunity.community.domain.reply.Reply;
+import com.example.mediacommunity.community.service.board.BoardCategoryService;
+import com.example.mediacommunity.community.service.board.BoardService;
+import com.example.mediacommunity.community.service.member.MemberService;
+import com.example.mediacommunity.community.service.reply.ReplyService;
+
+public class BeforeTest {
+     protected long createBoard(BoardService boardService, String categoryName, Member member) throws Exception {
+        Board board = Board.convertBoardRequestDtoToBoard(
+                new BoardRequestDto("newTitle", "newContent", categoryName, false),
+                member,
+                new BoardCategory(categoryName)
+        );
+        Board savedBoard = boardService.save(board);
+        return savedBoard.getId();
+    }
+
+    protected void createCategory(BoardCategoryService boardCategoryService, String categoryName) {
+        BoardCategory bc = new BoardCategory(categoryName);
+        boardCategoryService.save(bc);
+    }
+
+    protected Member saveMember(MemberService memberService) throws Exception {
+        return memberService.encodeAndSave(new SignUpDto("tester0", "password", "tester0"));
+    }
+
+    protected Reply createReply(ReplyService replyService, long boardId, String memberId) {
+        return replyService.reply(boardId, memberId, "newContent");
+    }
+}
