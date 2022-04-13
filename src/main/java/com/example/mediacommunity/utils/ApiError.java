5f63addcc2e8cc3c2482b487412dd35abf36cb67
Lee SeungHun <q6801@naver.com>
2022-04-13 16:28:42 +0900

    feat: api error 작성

---
+++ src/main/java/com/example/mediacommunity/utils/ApiError.java
@@ -0,0 +1,20 @@
+package com.example.mediacommunity.utils;
+
+import com.example.mediacommunity.Exception.CustomRuntimeException;
+import lombok.Getter;
+
+@Getter
+public class ApiError {
+    private String errorCode;
+    private String errorMessage;
+
+    public ApiError(CustomRuntimeException ex) {
+        this.errorCode = ex.getError().getCode();
+        this.errorMessage = ex.getError().getMessage();
+    }
+
+    public ApiError(String errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+}
