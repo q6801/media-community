b16e3bd1f45f0302816633737eab0beb10387c73
Lee SeungHun <q6801@naver.com>
2022-04-15 11:49:28 +0900

    feat: log를 추가

--- src/main/java/com/example/mediacommunity/utils/ApiUtils.java
+++ src/main/java/com/example/mediacommunity/utils/ApiUtils.java
@@ -7,15 +7,19 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ApiUtils {
     public static <T> ApiResult<T> success(T response) {
-        log.info("fuck");
+        log.info("successful response payload: {}", response);
         return new ApiResult<>(true, response, null);
     }
 
     public static <T> ApiResult<T> error(CustomRuntimeException throwable) {
-        return new ApiResult<>(false, null, new ApiError(throwable));
+        ApiError apiError = new ApiError(throwable);
+        log.info("error: {}", apiError);
+        return new ApiResult<>(false, null, apiError);
     }
 
     public static <T> ApiResult<T> error(String code, String message) {
-        return new ApiResult<>(false, null, new ApiError(code, message));
+        ApiError apiError = new ApiError(code, message);
+        log.info("error: {}", apiError);
+        return new ApiResult<>(false, null, apiError);
     }
 }
