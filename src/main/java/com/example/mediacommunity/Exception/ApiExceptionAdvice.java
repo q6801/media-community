54b50b421804de7c36074073009f22e7fe864402
Lee SeungHun <q6801@naver.com>
2022-04-15 11:46:32 +0900

    fix: 단순 ApiResult를 반환하면 http status가 200으로 response가 생성된다.

--- src/main/java/com/example/mediacommunity/Exception/ApiExceptionAdvice.java
+++ src/main/java/com/example/mediacommunity/Exception/ApiExceptionAdvice.java
@@ -2,14 +2,17 @@ package com.example.mediacommunity.Exception;
 
 import com.example.mediacommunity.utils.ApiResult;
 import com.example.mediacommunity.utils.ApiUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ApiExceptionAdvice {
 
     @ExceptionHandler({CustomRuntimeException.class})
-    public ApiResult<?> exceptionHandler(CustomRuntimeException ex) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResult exceptionHandler(CustomRuntimeException ex) {
         ApiResult<?> error = ApiUtils.error(ex);
         return error;
     }
