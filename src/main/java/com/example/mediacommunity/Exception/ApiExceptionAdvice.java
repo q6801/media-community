e454a1d54d47a56c73612ae5df361f50b73665b5
Lee SeungHun <q6801@naver.com>
2022-04-18 18:03:04 +0900

    feat: 사용자 입력 형식이 알맞지 않을 때 발생하는 methodArgumentNotValidException handler 추가

--- src/main/java/com/example/mediacommunity/Exception/ApiExceptionAdvice.java
+++ src/main/java/com/example/mediacommunity/Exception/ApiExceptionAdvice.java
@@ -3,6 +3,9 @@ package com.example.mediacommunity.Exception;
 import com.example.mediacommunity.utils.ApiResult;
 import com.example.mediacommunity.utils.ApiUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,7 +16,19 @@ public class ApiExceptionAdvice {
     @ExceptionHandler({CustomRuntimeException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiResult exceptionHandler(CustomRuntimeException ex) {
-        ApiResult<?> error = ApiUtils.error(ex);
-        return error;
+        return ApiUtils.error(ex);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResult handleValidationExceptions(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        StringBuilder sb = new StringBuilder();
+        for (FieldError fieldError: bindingResult.getFieldErrors()) {
+            sb.append("[").append(fieldError.getField()).append("]은(는) ");
+            sb.append(fieldError.getDefaultMessage());
+            sb.append(". ");
+        }
+        return ApiUtils.error("common-002", sb.toString());
     }
 }
